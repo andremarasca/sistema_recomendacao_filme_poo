60d7cda6d6a4328c63b03b6dbc1e6705be0a77a0
André Luiz Marasca <andremarasca@outlook.com>
2021-05-11 08:43:34 -0300

    selecao de target matrix e target user

--- main.py
+++ main.py
@@ -1,33 +1,72 @@
 from sistema_recomendacao_user_based import SistemaRecomendacaoUserBased
+from genero import Genero
+from occupation import Occupation
+from usuario import Usuario
+from filme import Filme
+from avaliacao import Avaliacao
+from read_dataset import read_dataset
+import pandas as pd
 
 srub = SistemaRecomendacaoUserBased(
     "ml-100k/u.user", "ml-100k/u.genre", "ml-100k/u.item", "ml-100k/u.data")
 
-# Acessar a primeira avaliação a lista do usuário 100, mostrar o movie_id e o rating
+# # Acessar a primeira avaliação a lista do usuário 100, mostrar o movie_id e o rating
 
-use_id = "100"
-user = srub.usuarios[use_id]
-aval = user.avaliacoes[0]
-rating = aval.rating
-movie_id = aval.filme.movie_id
+# user_id = "100"
+# user = srub.usuarios[user_id]
+# aval = user.avaliacoes[0]
+# rating = aval.rating
+# movie_id = aval.filme.movie_id
 
-print(f"Usuario {use_id} avaliou o filme {movie_id} com nota {rating}")
+# print(f"Usuario {user_id} avaliou o filme {movie_id} com nota {rating}")
 
-# Descobrir os generos do filme 123
+# # Descobrir os generos do filme 123
 
-movie_id = "123"
-movie = srub.filmes[movie_id]
-generos = movie.generos
-for genero in generos:
-    print(f"Filme {movie_id} tem genero {genero.nome}")
+# movie_id = "123"
+# movie = srub.filmes[movie_id]
+# generos = movie.generos
+# for genero in generos:
+#     print(f"Filme {movie_id} tem genero {genero.nome}")
 
-# Descobrir o id de todos os programadores
+# # Descobrir o id de todos os programadores
 
-ocupacao_programador = srub.ocupacoes["programmer"]
-programadores = ocupacao_programador.usuarios
-print("ID dos programadores:", *map(lambda x: x.user_id, programadores))
+# ocupacao_programador = srub.ocupacoes["programmer"]
+# programadores = ocupacao_programador.usuarios
+# print("ID dos programadores:", *map(lambda x: x.user_id, programadores))
 
-# Descobrir movie_id de todos os filmes de terror
+# # Descobrir movie_id de todos os filmes de terror
 
-horror = srub.generos["Horror"]
-print("ID dos filmes de Horror:", *map(lambda x: x.movie_id, horror.filmes))
+# horror = srub.generos["Horror"]
+# print("ID dos filmes de Horror:", *map(lambda x: x.movie_id, horror.filmes))
+
+# # Listar o nome dos filmes que são de Horror e ação
+
+# action = srub.generos["Action"].filmes
+# horror = srub.generos["Horror"].filmes
+
+
+# def intersection(lst1, lst2):
+#     lst3 = [value for value in lst1 if value in lst2]
+#     return lst3
+
+
+# action_horror = intersection(action, horror)
+
+# for filme in action_horror:
+#     print("Filme de Acao e Horror:", filme.movie_title)
+
+# # Mostrar todas as avaliações do usuário 1, mostrar o movie_id e o rating
+
+# user_id = "1"
+# user = srub.usuarios[user_id]
+
+# for aval in user.avaliacoes:
+#     rating = aval.rating
+#     movie_id = aval.filme.movie_id
+
+#     print(f"Usuario {user_id} avaliou o filme {movie_id} com nota {rating}")
+
+# %%
+
+target_matrix = srub.select_target_instances("1")
+target_user = srub.select_user_target("1")
--- sistema_recomendacao_user_based.py
+++ sistema_recomendacao_user_based.py
@@ -4,103 +4,22 @@ from usuario import Usuario
 from filme import Filme
 from avaliacao import Avaliacao
 from read_dataset import read_dataset
+import pandas as pd
+from sistema_recomendacao import SistemaRecomendacao
 
 
-class DadosNaoImportados(Exception):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
-
-
-class SistemaRecomendacaoUserBased:
+class SistemaRecomendacaoUserBased(SistemaRecomendacao):
     def __init__(self, user_filename, genre_filename, item_filename, data_filename) -> None:
-        self.usuarios = {}
-        self.filmes = {}
-        self.generos = {}
-        self.ocupacoes = {}
+        super().__init__(user_filename, genre_filename, item_filename, data_filename)
 
-        user_header = "user_id|age|gender|occupation|zip_code"
-        user_header = user_header.split("|")
-        dataset_user = read_dataset(
-            user_filename, sep="|", header=user_header)
+    def select_target_instances(self, target_movie_id):
 
-        genre_header = "genre|genre_id"
-        genre_header = genre_header.split("|")
-        dataset_genre = read_dataset(
-            genre_filename, sep="|", header=genre_header)
+        # self.filmes[target_movie_id] o obj filme de target_movie_id
+        # self.filmes[target_movie_id].avaliacoes é um dict e a chave é o user_id
+        target_users = self.filmes[target_movie_id].avaliacoes.keys()
 
-        item_header = "movie_id|movie_title|release_date|video_release_date|IMDb_URL|unknown|Action|Adventure|Animation|Children's|Comedy|Crime|Documentary|Drama|Fantasy|Film-Noir|Horror|Musical|Mystery|Romance|Sci-Fi|Thriller|War|Western"
-        item_header = item_header.split("|")
-        dataset_movie = read_dataset(
-            item_filename, sep="|", header=item_header)
+        # o primeiro indice do pandas são as colunas, pode-se passar uma lista para filtrar
+        return self.rating_matrix[target_users]
 
-        data_header = "user_id|item_id|rating|timestamp"
-        data_header = data_header.split("|")
-        dataset_data = read_dataset(
-            data_filename, sep="\t", header=data_header)
-
-        self.importar_dados_usuarios(dataset_user)
-        self.importar_dados_genero(dataset_genre)
-        self.importar_dados_filmes(dataset_movie)
-        self.importar_dados_avaliacoes(dataset_data)
-
-    def inserir_occupation(self, occupation: str):
-        if occupation not in self.ocupacoes:
-            self.ocupacoes[occupation] = Occupation(occupation)
-
-    def inserir_genre(self, genre: str):
-        if genre not in self.generos:
-            self.generos[genre] = Genero(genre)
-
-    def importar_dados_genero(self, dataset_genre):
-        for genre in dataset_genre["genre"]:
-            self.inserir_genre(genre)
-
-    def importar_dados_filmes(self, dataset_movie):
-        "movie_id|movie_title|release_date|video_release_date|IMDb_URL"
-        for idx in range(len(dataset_movie["movie_id"])):
-            movie_id = dataset_movie["movie_id"][idx]
-            movie_title = dataset_movie["movie_title"][idx]
-            release_date = dataset_movie["release_date"][idx]
-            video_release_date = dataset_movie["video_release_date"][idx]
-            IMDb_URL = dataset_movie["IMDb_URL"][idx]
-
-            movie = Filme(movie_id, movie_title, release_date,
-                          video_release_date, IMDb_URL)
-
-            for genre in self.generos:
-                if dataset_movie[genre][idx] == "1":
-                    obj_genre: Genero = self.generos[genre]
-                    movie.inserir_genero(obj_genre)
-                    obj_genre.inserir_filme(movie)
-
-            self.filmes[movie_id] = movie
-
-    def importar_dados_usuarios(self, dataset_user):
-        for idx in range(len(dataset_user["user_id"])):
-            user_id = dataset_user["user_id"][idx]
-            age = dataset_user["age"][idx]
-            gender = dataset_user["gender"][idx]
-            occupation = dataset_user["occupation"][idx]
-            zip_code = dataset_user["zip_code"][idx]
-
-            self.inserir_occupation(occupation)
-            obj_occupation: Occupation = self.ocupacoes[occupation]
-
-            user = Usuario(user_id, int(age), gender, obj_occupation, zip_code)
-            obj_occupation.inserir_usuario(user)
-
-            self.usuarios[user_id] = user
-
-    def importar_dados_avaliacoes(self, dataset_data):
-        for idx in range(len(dataset_data["user_id"])):
-            user_id = dataset_data["user_id"][idx]
-            item_id = dataset_data["item_id"][idx]
-            rating = dataset_data["rating"][idx]
-            timestamp = dataset_data["timestamp"][idx]
-
-            usuario: Usuario = self.usuarios[user_id]
-            filme: Filme = self.filmes[item_id]
-
-            avaliacao = Avaliacao(rating, timestamp, usuario, filme)
-            usuario.inserir_avaliacao(avaliacao)
-            filme.inserir_avaliacao(avaliacao)
+    def select_user_target(self, target_user_id):
+        return self.rating_matrix[target_user_id]
